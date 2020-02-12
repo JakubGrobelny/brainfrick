8a847ef8829b74b87c4c38678561c8a3f5b75e0a
Jakub Grobelny <kubissus@gmail.com>
2020-02-12 02:12:47 +0100

    Fixed a bug in the optimizer

--- src/Optimizer.hs
+++ src/Optimizer.hs
@@ -9,6 +9,7 @@ where
 
 import           Data.Either
 import qualified Instruction                   as I
+import Data.List
 
 type Offset = Int
 type Value = Int
@@ -85,7 +86,7 @@ optimizerPass xs = optimize $ unwrap xs
         Loop body         : xs -> case optimizeLoop body of
             Done       instr -> (instr :) <$> optimize xs
             InProgress instr -> InProgress $ instr : xs
-        MultiplyAdd 0 offset : xs-> InProgress $ Clear offset : xs
+        MultiplyAdd 0 offset : xs -> InProgress $ Clear offset : xs
         Add n offset : Shift m : xs ->
             InProgress $ Shift m : Add n (offset - m) : xs
         Add n offset0 : Add m offset1 : xs | offset0 == offset1 ->
@@ -98,14 +99,17 @@ optimizerPass xs = optimize $ unwrap xs
     optimizeLoop :: [OptimizedInstruction] -> Result OptimizedInstruction
     optimizeLoop xs = case optimize xs of
         Done xs -> if isSimple xs && goesToMinusOne xs
-            then InProgress . Block . reverse $ map addToSet xs
+            then InProgress . Block . sortBy ordering $ map addToSet xs
             else Done $ Loop xs
         InProgress xs -> InProgress $ Loop xs
       where
+        ordering :: OptimizedInstruction -> OptimizedInstruction -> Ordering
+        ordering (Clear 0) _ = GT
+        ordering _ _ = EQ
         addToSet :: OptimizedInstruction -> OptimizedInstruction
-        addToSet (Add (-1) 0) = Clear 0
-        addToSet (Add n offset) = MultiplyAdd n offset
-        addToSet i = i
+        addToSet (Add (-1) 0     ) = Clear 0
+        addToSet (Add n    offset) = MultiplyAdd n offset
+        addToSet i                 = i
         isSimple :: [OptimizedInstruction] -> Bool
         isSimple = all $ \case
             Add _ _ -> True
